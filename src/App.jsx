e946223770470bf793e2be72802481ac2b49ace7
xshuky <itsjuneforwork@gmail.com>
2023-03-10 14:20:51 +0100

    remove app.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css';
 import { Header } from './components/header/Header';
 import { Hangman } from './components/hangman/Hangman';
 import { Keyboard } from './components/keyboard/Keyboard';
