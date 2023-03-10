f60d7652d1997674dec8d7d67e54f182dfe48618
xshuky <itsjuneforwork@gmail.com>
2023-03-10 12:00:55 +0100

    add header style

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import { Header } from './components/header/Header';
 import { Hangman } from './components/hangman';
 
 const App = () => {
   return (
     <div className="App">
+      <Header />
       <Hangman />
     </div>
   );
