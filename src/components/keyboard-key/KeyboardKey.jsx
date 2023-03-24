a89a6b5186920411114bedfbfc509ac2d3280bea
xshuky <itsjuneforwork@gmail.com>
2023-03-24 16:20:04 +0100

    add function press

--- src/components/keyboard-key/KeyboardKey.jsx
+++ src/components/keyboard-key/KeyboardKey.jsx
@@ -1,10 +1,22 @@
 import { ReactComponent as KeySvg } from '../../assets/key.svg';
+import React, { useState } from 'react';
 import './KeyboardKey.css';
 
 export const Key = ({ letter }) => {
+  const [pressed, setPressed] = useState(false);
+
+  function changeClass() {
+    setPressed(true);
+    setTimeout(() => {
+      setPressed(false);
+    }, 300);
+  }
+
+  const className = pressed ? 'key__svg--pressed' : 'key__svg';
+
   return (
     <div className='key'>
-        <KeySvg className='key__svg'/>
+        <KeySvg className={className} onClick={changeClass}/>
         <p className='key__letter'>{letter}</p>
     </div>
   );
