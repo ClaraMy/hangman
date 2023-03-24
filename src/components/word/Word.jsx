bcee16d416f2c54a674964e4ba53e4b3c1fa488c
xshuky <itsjuneforwork@gmail.com>
2023-03-24 17:48:34 +0100

    remove word letter jsx content

--- src/components/word/Word.jsx
+++ src/components/word/Word.jsx
@@ -1,26 +1,19 @@
-import { WordLetter } from '../word-letter/WordLetter';
 import './Word.css';
 
-export const Word = ({ word }) => {
+export const Word = ({ word, lettersFound }) => {
     // Fonction pour transformer la chaîne de caractère en un tableau avec chacune des lettres
-    let chars = word.split('');
-    const statut = "visible";
-    const letter = chars.map((letter) =>
-        <WordLetter statut={statut} letter={letter} />
-    );
-
-    // const onClick = ( keyLetter ) => {
-    //     chars.map((wordLetter) => 
-    //         if (keyLetter === wordLetter) {
-    //             statut = "visible";
-    //         }
-    //     )
-    // }
-
+    let wordLetters = word.split('');
 
   return (
     <div className='word'>
-        {letter} 
+        {wordLetters.map((letter, index) => (
+          <span
+            key={index}
+            className={`letter ${lettersFound.includes(letter) ? 'trouvee' : ''}`}
+          >
+            {lettersFound.includes(letter) ? letter : '_'}
+          </span>
+        ))}
     </div>
   );
 };
