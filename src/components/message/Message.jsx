349f3974da8d324039c2cea7905b491a8be1bbd8
xshuky <itsjuneforwork@gmail.com>
2023-03-26 20:30:30 +0200

    add text

--- src/components/message/Message.jsx
+++ src/components/message/Message.jsx
@@ -1,7 +1,7 @@
 import './Message.css';
 
-export const Message = ({ text, word }) => {
+export const Message = ({ text, word, win }) => {
   return (
-    <p className='modal__text'>{text}</p>
+    <p className='modal__text'>{win ? `vous avez survecu a la pendaison car vous avez trouve le mot ${word}! (ca ne se passera pas comme ca a chaque fois)` : "oh non vous etes mort :("}</p>
   );
 };
