7e03235db33ead70b25411aea7ac7d9f57f002b9
xshuky <itsjuneforwork@gmail.com>
2023-03-26 22:41:34 +0200

    remove message

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,14 +1,13 @@
 import './Modal.css';
 import { ReactComponent as ModalSvg } from '../../assets/modal.svg';
 import { Button } from '../button/Button';
-import { Message } from '../message/Message';
 
 export const Modal = ({ word, win, onClose }) => {
   return (
     <div className='modal'>
         <ModalSvg />
         <div className="modal__container">
-          <Message word={word} win={win}/>
+          <p className='modal__text'>{win ? `vous avez survecu a la pendaison car vous avez trouve le mot ${word}! (ca ne se passera pas comme ca a chaque fois)` : "oh non vous etes mort :("}</p>
           <Button onClose={onClose} win={win} />
         </div>
     </div>
