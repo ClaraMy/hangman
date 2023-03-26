f95a00dd33d2df9d6c295a0e75e21034e1ac0e0d
xshuky <itsjuneforwork@gmail.com>
2023-03-26 18:51:43 +0200

    add button

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,11 +1,14 @@
 import './Modal.css';
 import { ReactComponent as ModalSvg } from '../../assets/modal.svg';
+import { Button } from '../button/Button';
 
 export const Modal = ({ state }) => {
   return (
     <div className='modal'>
         <ModalSvg />
         <p className="modal__text"></p>
+        <Button text={text1} subtext={subtext1} />
+        <Button text={text2} subtext={subtext2} />
     </div>
   );
 };
