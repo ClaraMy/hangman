305919e0267e27fa89377a6d121cea234847692b
xshuky <itsjuneforwork@gmail.com>
2023-03-26 18:46:52 +0200

    add modal svg and text

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,9 +1,11 @@
 import './Modal.css';
+import { ReactComponent as ModalSvg } from '../../assets/modal.svg';
 
 export const Modal = ({ state }) => {
   return (
     <div className='modal'>
-        
+        <ModalSvg />
+        <p className="modal__text"></p>
     </div>
   );
 };
