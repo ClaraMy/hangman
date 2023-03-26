849bd7bbca925b93935f03cae52806aaed65c034
xshuky <itsjuneforwork@gmail.com>
2023-03-26 19:56:50 +0200

    add onClick in button

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -2,13 +2,12 @@ import './Modal.css';
 import { ReactComponent as ModalSvg } from '../../assets/modal.svg';
 import { Button } from '../button/Button';
 
-export const Modal = ({ word, state, textReplay, textHome, subtextReplay, subtextHome }) => {
+export const Modal = ({ word, state, textReplay, textHome, subtextReplay, subtextHome, onClose }) => {
   return (
     <div className='modal'>
         <ModalSvg />
-        <p className="modal__text"></p>
         <Button text={textReplay} subtext={subtextReplay} />
-        <Button text={textHome} subtext={subtextHome} />
+        <Button onClick={onClose} text={textHome} subtext={subtextHome} />
     </div>
   );
 };
