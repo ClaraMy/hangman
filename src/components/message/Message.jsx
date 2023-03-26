e324f26f8dd2fb6574f0369a82c895be8d11eed2
xshuky <itsjuneforwork@gmail.com>
2023-03-26 20:32:51 +0200

    change className

--- src/components/message/Message.jsx
+++ src/components/message/Message.jsx
@@ -2,6 +2,6 @@ import './Message.css';
 
 export const Message = ({ word, win }) => {
   return (
-    <p className='modal__text'>{win ? `vous avez survecu a la pendaison car vous avez trouve le mot ${word}! (ca ne se passera pas comme ca a chaque fois)` : "oh non vous etes mort :("}</p>
+    <p className='message'>{win ? `vous avez survecu a la pendaison car vous avez trouve le mot ${word}! (ca ne se passera pas comme ca a chaque fois)` : "oh non vous etes mort :("}</p>
   );
 };
