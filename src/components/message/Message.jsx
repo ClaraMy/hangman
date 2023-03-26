e1cadec656a8503b7b79cb33de47307b7ed36f40
xshuky <itsjuneforwork@gmail.com>
2023-03-26 22:44:50 +0200

    remove message content

--- src/components/message/Message.jsx
+++ src/components/message/Message.jsx
@@ -1,7 +1,7 @@
 import './Message.css';
 
-export const Message = ({ word, win }) => {
+export const Message = ({  }) => {
   return (
-    <p className='message'>{win ? `vous avez survecu a la pendaison car vous avez trouve le mot ${word}! (ca ne se passera pas comme ca a chaque fois)` : "oh non vous etes mort :("}</p>
+    <p className='message'></p>
   );
 };
