869b99b1b0c124e6f21da866181def3af86a01d5
xshuky <itsjuneforwork@gmail.com>
2023-03-26 18:55:17 +0200

    add addText function

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -3,6 +3,23 @@ import { ReactComponent as ModalSvg } from '../../assets/modal.svg';
 import { Button } from '../button/Button';
 
 export const Modal = ({ state }) => {
+  const addText = () => {
+    if (state === "win") {
+      text1 = "risquer encore sa vie";
+      text2 = "partir de cet enfer";
+    }
+
+    if (state === "lose") {
+      text1 = "mourir de nouveau";
+      text2 = "accepter la mort";
+    }
+
+    subtext1 = "(rejouer)";
+    subtext2 = "(retourner au menu)";
+  }
+
+  addText();
+
   return (
     <div className='modal'>
         <ModalSvg />
