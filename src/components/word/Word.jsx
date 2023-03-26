3852d2a70f0135b96c9adf1fe8faefb08dc1059a
xshuky <itsjuneforwork@gmail.com>
2023-03-26 19:47:26 +0200

    add useEffect

--- src/components/word/Word.jsx
+++ src/components/word/Word.jsx
@@ -1,8 +1,20 @@
+import { useEffect } from 'react';
 import './Word.css';
 
 export const Word = ({ word, lettersFound, onWordFound }) => {
     // Fonction pour transformer la chaîne de caractère en un tableau avec chacune des lettres
     let wordLetters = word.split('');
+
+    // fonction pour savoir si toutes les lettres du mot ont été trouvées
+    const isWordFound = word.split("").every((letter) =>
+      lettersFound.includes(letter)
+    );
+
+    useEffect(() => {
+      if (isWordFound) {
+        onWordFound();
+      }
+     }, [isWordFound]);
 
   return (
     <div className='word'>
