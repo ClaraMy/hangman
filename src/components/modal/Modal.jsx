8181d115a930dbfe53ecabdf5e2a2930259f91bc
xshuky <itsjuneforwork@gmail.com>
2023-03-26 19:33:03 +0200

    add word props

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -2,7 +2,7 @@ import './Modal.css';
 import { ReactComponent as ModalSvg } from '../../assets/modal.svg';
 import { Button } from '../button/Button';
 
-export const Modal = ({ state, textReplay, textHome, subtextReplay, subtextHome }) => {
+export const Modal = ({ word, state, textReplay, textHome, subtextReplay, subtextHome }) => {
   return (
     <div className='modal'>
         <ModalSvg />
