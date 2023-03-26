4da78602024aebe02352e5be195a33dedf329165
xshuky <itsjuneforwork@gmail.com>
2023-03-26 20:46:04 +0200

    add button class

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -2,7 +2,7 @@ import './Button.css';
 
 export const Button = ({ win }) => {
   return (
-    <div>
+    <div className='button'>
         <p className="button__text">{win ? `risquer sa vie de nouveau` : "mourir de nouveau"}</p>
         <p className="button__subtext">(rejouer)</p>
     </div>
