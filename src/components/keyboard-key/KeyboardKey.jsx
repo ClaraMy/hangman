09f9ae4a291b30f73eb07302b25e66347e59f1ff
xshuky <itsjuneforwork@gmail.com>
2023-03-27 17:27:11 +0200

    add comment

--- src/components/keyboard-key/KeyboardKey.jsx
+++ src/components/keyboard-key/KeyboardKey.jsx
@@ -3,10 +3,14 @@ import React, { useEffect, useState } from 'react';
 import './KeyboardKey.css';
 
 export const Key = ({ letter, onClick, lettersFound }) => {
+  // -- CONSTANTES --
   const [isPressed, setIsPressed] = useState(false);
   const [isClicked, setIsClicked] = useState(false);
   const [color, setColor] = useState("#FFFFFF");
+  const className = `key${isPressed ? ' key--pressed' : ''}`;
 
+  // -- FONCTIONS --
+  // fonction pour changer la classe de la touche au clic
   const changeClass = () => {
     setIsPressed(true);
     setTimeout(() => {
@@ -14,24 +18,26 @@ export const Key = ({ letter, onClick, lettersFound }) => {
     }, 300);
   }
 
-  const className = `key${isPressed ? ' key--pressed' : ''}`;
+  // fonction pour changer l'arrière-plan couleur de la touche
+  const changeColor = () => {
+    onClick(letter);
+    setIsClicked(true);
+  }
 
+  // -- USEEFFECT --
+  // faire qu'à chaque clic ça met à jour la couleur
   useEffect(() => {
     if (isClicked) {
       setColor(lettersFound.includes(letter) ? "#94D9B4" : "#CDCDCD");
     }
   }, [isClicked]);
 
+  // faire qu'à chaque fois que le tableau lettersFound est vide (quand on rejoue), cela réinitialise la couleur des touches
   useEffect(() => {
     if (lettersFound.length === 0) {
       setColor("#FFFFFF");
     }
   }, [lettersFound]);
-
-  const changeColor = () => {
-    onClick(letter);
-    setIsClicked(true);
-  }
 
   return (
     <div className={className} onClick={() => {
